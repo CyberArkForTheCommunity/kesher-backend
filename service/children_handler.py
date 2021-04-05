fc2c298c0a5364f6361854193bfda95bd4384a61
nirz <nir.zamir@cyberark.com>
2021-04-05 16:42:46 +0300

    add get children stub

--- service/children_handler.py
+++ service/children_handler.py
@@ -1,26 +1,28 @@
+import json
 from http import HTTPStatus
-from aws_lambda_context import LambdaContext
-from pydantic import ValidationError
-from logging import Logger
+
 from service.response_utils import build_error_response, build_response
 
+from aws_lambda_powertools.logging import logger
+from aws_lambda_context import LambdaContext
+from pydantic import ValidationError
+from aws_lambda_powertools import Logger
+from service.models.child import Child
+
+logger = Logger()
 
 # GET /api/children
-@Logger.inject_lambda_context(log_event=True)
+@logger.inject_lambda_context(log_event=True)
 def get_children(event: dict, context: LambdaContext) -> dict:
-    # if "pathParameters" not in event or "name" not in event["pathParameters"]:
-    #     return build_response(HTTPStatus.BAD_REQUEST, "")
-
     try:
-        # name: str = event["pathParameters"]["name"]
-        # #  Note: dates should be read from the database
-        # now: Decimal = Decimal(datetime.now().timestamp())
-        # item: Kesher = Kesher(name=name, created_date=now, updated_date=now)
-        # if item is None:
-        #     return _build_response(HTTPStatus.NOT_FOUND, body="{ 'message': 'item was not found' }")
-        # body = item.json()
-        return build_response(HTTPStatus.OK, "")
+        children = [
+            Child(id='12345', first_name='Yeled', last_name='One').dict(),
+            Child(id='436346', first_name='Kid', last_name='Two').dict()
+        ]
+        body = json.dumps({'Children': children})
+        return build_response(HTTPStatus.OK, body)
     except (ValidationError, TypeError) as err:
         return build_error_response(err, HTTPStatus.BAD_REQUEST)
     except Exception as err:
         return build_error_response(err)
+
