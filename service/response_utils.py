82a4eeadbe6db1fecc7e352ffe31d07115f82df6
nirz <nir.zamir@cyberark.com>
2021-04-05 13:32:27 +0300

    update response_utils

--- service/response_utils.py
+++ service/response_utils.py
@@ -3,14 +3,12 @@ from http import HTTPStatus
 from logging import Logger
 
 
-logger = Logger()
 
 def build_response(http_status: HTTPStatus, body: str) -> dict:
     return {'statusCode': http_status, 'headers': {'Content-Type': 'application/json'}, 'body': body}
 
 
 def build_error_response(err, status: HTTPStatus = HTTPStatus.INTERNAL_SERVER_ERROR) -> dict:
-    logger.error(str(err))
     return {
         'statusCode': status,
         'headers': {
