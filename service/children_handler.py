fef2326f7a31452246845494cd731a0acb99f988
nirz <nir.zamir@cyberark.com>
2021-04-05 18:54:58 +0300

    add daily report stub

--- service/children_handler.py
+++ service/children_handler.py
@@ -1,5 +1,8 @@
+from datetime import datetime
+from decimal import Decimal
 import json
 from http import HTTPStatus
+from service.models.daily_report import DailyReportAdd, DailyReportGet
 
 from service.response_utils import build_error_response, build_response
 
@@ -19,7 +22,46 @@ def get_children(event: dict, context: LambdaContext) -> dict:
             Child(id='12345', first_name='Yeled', last_name='One').dict(),
             Child(id='436346', first_name='Kid', last_name='Two').dict()
         ]
-        body = json.dumps({'Children': children})
+        body = json.dumps({'children': children})
+        return build_response(HTTPStatus.OK, body)
+    except (ValidationError, TypeError) as err:
+        return build_error_response(err, HTTPStatus.BAD_REQUEST)
+    except Exception as err:
+        return build_error_response(err)
+
+
+# POST /api/children/{child_id}/daily-reports
+@logger.inject_lambda_context(log_event=True)
+def add_child_report(event: dict, context: LambdaContext) -> dict:
+    if "pathParameters" not in event or "child_id" not in event["pathParameters"]:
+        return build_response(HTTPStatus.BAD_REQUEST, "")
+
+    try:
+        child_id: str = event["pathParameters"]["child_id"]
+        daily_report = DailyReportAdd.parse_raw(event["body"])
+        # TODO get from DB
+        return build_response(http_status=HTTPStatus.CREATED, body=daily_report.json())
+    except (ValidationError, TypeError) as err:
+        return build_error_response(err, HTTPStatus.BAD_REQUEST)
+    except Exception as err:
+        return build_error_response(err)
+   
+# GET /api/children/{child_id}/daily-reports
+@logger.inject_lambda_context(log_event=True)
+def get_child_reports(event: dict, context: LambdaContext) -> dict:
+    if "pathParameters" not in event or "child_id" not in event["pathParameters"]:
+        return build_response(HTTPStatus.BAD_REQUEST, event)
+
+    try:
+        child_id: str = event["pathParameters"]["child_id"]
+        now: Decimal = Decimal(datetime.now().timestamp())
+        daily_reports = [
+            DailyReportGet(child_id=child_id, value="Itamar ate lunch", category_id=1234, subcategory_id=4635, sender_id=23432, timestamp=now).dict(),
+            DailyReportGet(child_id=child_id, value="Itamar ate dinner", category_id=1234, subcategory_id=235235, sender_id=23432, timestamp=now).dict()
+        ]
+        if daily_reports is None:
+            return build_response(HTTPStatus.NOT_FOUND, body="")
+        body = json.dumps({'daily_reports': daily_reports})
         return build_response(HTTPStatus.OK, body)
     except (ValidationError, TypeError) as err:
         return build_error_response(err, HTTPStatus.BAD_REQUEST)
---
+++ service/models/daily_report.py
@@ -0,0 +1,11 @@
+from typing import Optional
+from pydantic import BaseModel
+
+class DailyReportAdd(BaseModel):
+    value: str
+    category_id: Optional[int]
+    subcategory_id: Optional[int]
+
+class DailyReportGet(DailyReportAdd):
+    sender_id: int
+    timestamp: int
